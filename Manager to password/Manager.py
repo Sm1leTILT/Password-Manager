57e4fd62d776c83559137f9eef639f2fd9935362
Sm1leTILT <128966614+Sm1leTILT@users.noreply.github.com>
2023-05-21 13:26:52 +0300

    Update Manager.py

--- Manager to password/Manager.py
+++ Manager to password/Manager.py
@@ -1,15 +1,24 @@
+Просмотр файла: c:\Users\Teacher\Desktop\PasswordManager\app.py
 from PyQt5 import QtWidgets, QtGui, QtCore
+import json
+import keyring
+# idkru.pythonanywhere.com/code/dezine
 
-class MyWindow(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.initUI()
-        self.setWindowTitle('Password Manager')
-        self.resize(800, 600)
+        self.setWindowTitle('PasswordManager')
+        self.resize(600, 480)
+
+        self.db = DataBase()
+
+        self.add_pwd_win = PasswordWindow(self.db, self)
+        self.btn_add_pwd.clicked.connect(self.add_pwd_win.show)
 
     def initUI(self):
-        self.btn_add_pwd = QtWidgets.QPushButton('Add Password')
+        self.btn_add_pwd = QtWidgets.QPushButton('Добавить')
         self.pwd_list = QtWidgets.QListWidget()
 
         h_layout = QtWidgets.QHBoxLayout()
@@ -23,13 +32,20 @@ class MyWindow(QtWidgets.QWidget):
 
         self.setLayout(v_layout)
 
-class PasswordWindow(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super().__init__()
+    def update_list(self):
+        self.pwd_list.clear()
+        data = self.db.get_list()
+        self.pwd_list.addItems(data)
 
+
+class PasswordWindow(QtWidgets.QWidget):
+    def __init__(self, db, mw):
+        super().__init__()
+        self.db = db
+        self.mw = mw
         self.initUI()
-        self.setWindowTitle('Password Window')
-        self.resize(200, 100)
+
+        self.btn_save.clicked.connect(self.add_password)
 
     def initUI(self):
         v_layout = QtWidgets.QVBoxLayout()
@@ -37,39 +53,82 @@ class PasswordWindow(QtWidgets.QWidget):
         h_1 = QtWidgets.QHBoxLayout()
         h_2 = QtWidgets.QHBoxLayout()
         h_3 = QtWidgets.QHBoxLayout()
-        h_4 = QtWidgets.QHBoxLayout()
-
-        self.btn_edit = QtWidgets.QPushButton('Edit Password')
-        self.btn_close = QtWidgets.QPushButton('Close')
+        
+        self.btn_save = QtWidgets.QPushButton('Сохранить')
+        self.btn_close = QtWidgets.QPushButton('Закрыть')
         self.btn_copy = QtWidgets.QPushButton('Copy')
 
-        self.servise_edit = QtWidgets.QLineEdit()
+        self.service_edit = QtWidgets.QLineEdit()
         self.login_edit = QtWidgets.QLineEdit()
         self.pwd_edit = QtWidgets.QLineEdit()
 
         self.pwd_edit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
 
-        servise_lbl = QtWidgets.QLabel('Service:')
-        login_lbl = QtWidgets.QLabel('Login:')
-        pwd_lbl = QtWidgets.QLabel('Password:')
+        service_lbl = QtWidgets.QLabel('Название сервиса')
+        login_lbl = QtWidgets.QLabel('Логин')
+        pwd_lbl = QtWidgets.QLabel('Пароль')
 
-        h_1.addWidget(servise_lbl, stretch=3)
-        h_1.addWidget(self.servise_edit, stretch=5)
+        h_1.addWidget(service_lbl, stretch=3)
+        h_1.addWidget(self.service_edit, stretch=5)
 
         h_2.addWidget(login_lbl, stretch=3)
-        h_2.addWidget(self.servise_edit, stretch=5)
+        h_2.addWidget(self.login_edit, stretch=5)
 
         h_3.addWidget(pwd_lbl, stretch=3)
-        h_3.addWidget(self.servise_edit, stretch=5)
+        h_3.addWidget(self.pwd_edit, stretch=5)
 
         v_layout.addLayout(h_1)
         v_layout.addLayout(h_2)
         v_layout.addLayout(h_3)
+        v_layout.addWidget(self.btn_save, alignment=QtCore.Qt.AlignCenter)
 
         self.setLayout(v_layout)
 
+    def add_password(self):
+        service = self.service_edit.text()
+        login = self.login_edit.text()
+        password = self.pwd_edit.text()
+        self.db.set_password(service, login, password)
+        self.service_edit.clear()
+        self.login_edit.clear()
+        self.pwd_edit.clear()
+        self.hide()
+        self.mw.update_list()
+
+
+class DataBase:
+    def __init__(self):
+        self.filename = 'services.json'
+        try:
+            with open(self.filename, 'r') as file:
+                self.data = json.load(file)
+        except:
+            open(self.filename, 'w')
+            self.data = dict()
+
+    def set_password(self, service, login, password):
+        keyring.set_password(service, login, password)
+        self.data[service] = login
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+        
+    def get_password(self, service, login):
+        return keyring.get_password(service, login)
+
+    def get_list(self):
+        result = []
+        for key in self.data:
+            text = key + ' | ' + self.data[key]
+            result.append(text)
+        return result
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    window = MyWindow()
-    window.show()
+    mw = MainWindow()
+    mw.show()
     app.exec()
+
+
+# https://ibb.co/bBZW4pH
+# https://ibb.co/b6qHmKd
